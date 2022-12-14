48b59020560f3992f077b41ddc359df04a71a724
mdshahid <87535723+shahid1330@users.noreply.github.com>
2022-12-14 23:49:32 +0530

    Add files via upload
    Question 2:- Write a program that reads numbers are entered one after another as input, terminated by a value 0. Add those numbers and display the sum.

---
+++ Week1_Q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,num=1;
+	for(int i=i;num!=0;i++)
+	{
+		cout<<"Enter the number=";
+		cin>>num;
+		x+=num;
+	}
+	cout<<x;
+	return 0;
+}
